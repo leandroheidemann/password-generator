dd67728892af8565f4d48f25349bd69207dbd558
Leandro Heidemann da Silva <leandrohds_bn@hotmail.com>
2022-03-22 23:50:05 -0300

    Create password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'password-generator';
+  private chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  passwordLength = 12;
+  password = "";
+
+  @ViewChild('iptPassword') private iptPassword!: ElementRef<HTMLInputElement>
+
+  constructor(private toast: MessageService) {
+  }
+
+  generatePassword(): void {
+    this.password = "";
+
+    for (var i = 0; i <= this.passwordLength; i++) {
+      const randomNumber = Math.floor(Math.random() * this.chars.length);
+      this.password += this.chars.substring(randomNumber, randomNumber + 1);
+    }
+  }
+
+  async copyPassword(): Promise<void> {
+    await navigator.clipboard.writeText(this.password);
+
+    this.toast.add({
+      key: 'toast',
+      severity: 'success',
+      summary: 'Sucesso',
+      detail: 'Senha copiada'
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {InputTextModule} from "primeng/inputtext";
+import {ButtonModule} from "primeng/button";
+import {FormsModule} from "@angular/forms";
+import {SliderModule} from "primeng/slider";
+import {ToastModule} from "primeng/toast";
+import {MessageService} from "primeng/api";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    ButtonModule,
+    FormsModule,
+    SliderModule,
+    ToastModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
